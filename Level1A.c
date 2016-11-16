a5cfad03dadafabde83a3c5593f276f707c1bece
James Grant <dynamicll@protonmail.ch>
2016-11-16 11:36:41 +1100

    Create Level1A.c

---
+++ Level1A.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char* exec = "/bin/sh";
+
+void win(char* cmd)
+{
+	system(cmd);
+}
+
+void print_name(char* input)
+{
+	char buf[15];
+	strcpy(buf, input);
+	printf("Hello %s\n", buf);
+}
+
+int main(int argc, char** argv)
+{
+	if(argc != 2)
+	{
+		printf("usage:\n%s string\n", argv[0]);
+		exit(1);
+	}
+
+	print_name(argv[1]);
+
+	return 0;
+}
